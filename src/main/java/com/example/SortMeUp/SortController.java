867f301a4ab6a9eba290d58c5d1b9146c81503b5
aksrajvanshi <aksrajvanshi@gmail.com>
2019-02-27 03:53:44 +0900

    Sort me up! Revision 3. Get data from page through ajax call.

--- src/main/java/com/example/SortMeUp/SortController.java
+++ src/main/java/com/example/SortMeUp/SortController.java
@@ -3,13 +3,26 @@ package com.example.SortMeUp;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ComponentScan
 @Controller
 public class SortController {
 
     @RequestMapping(value = "/")
-    public String sortNumbers(){
+    public String init(){
+
+        return "sortPage";
+    }
+
+
+    @RequestMapping(value = "/sort", method = RequestMethod.GET)
+    @ResponseBody
+    public String sortNumbers(@RequestParam("id") String id){
+
+        System.out.println(" i am in the sort numbers function! " + id);
 
         return "sortPage";
     }
