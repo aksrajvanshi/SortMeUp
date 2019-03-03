c4e79edfb3d7a78a407b3cfed6f90e914a4bf3b1
aksrajvanshi <aksrajvanshi@gmail.com>
2019-03-03 23:15:09 +0900

    Responding successfully to ajax call.

--- src/main/java/com/example/SortMeUp/SortController.java
+++ src/main/java/com/example/SortMeUp/SortController.java
@@ -2,10 +2,7 @@ package com.example.SortMeUp;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @ComponentScan
 @Controller
@@ -24,7 +21,7 @@ public class SortController {
 
         System.out.println(" i am in the sort numbers function! " + id + " with array values " + arr );
 
-        return "sortPage";
+        return "data entered is now sorted";
     }
 
 }
