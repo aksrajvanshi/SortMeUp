37b4b9901b76ad0803875d779f648c5f0b810fed
aksrajvanshi <aksrajvanshi@gmail.com>
2019-03-23 01:13:36 +0900

    added bubblesort algorithm

--- src/main/java/com/example/SortMeUp/SortingAlgoBehaviors/bubbleSort.java
+++ src/main/java/com/example/SortMeUp/SortingAlgoBehaviors/bubbleSort.java
@@ -6,6 +6,26 @@ public class bubbleSort implements SortingBehavior {
     @Override
     public int[] sort(int[] arrayToBeSorted) {
 
+        if(arrayToBeSorted.length == 0){
+            return arrayToBeSorted;
+        }
+
+        for(int i=0; i< arrayToBeSorted.length-1; i++){
+
+            for(int j=0; j < arrayToBeSorted.length-1-i; j++) {
+
+                if(arrayToBeSorted[j] > arrayToBeSorted[j+1]){
+
+                    int temp = arrayToBeSorted[j];
+                    arrayToBeSorted[j] = arrayToBeSorted[j+1];
+                    arrayToBeSorted[j+1] = temp;
+
+                }
+
+            }
+        }
+
+
         return arrayToBeSorted;
     }
 }
